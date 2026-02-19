f8ccba7cfe46fd84486183b6b22998ca2b187854
seeker-zuo <seeker-zuo@outlook.com>
2026-02-19 10:55:48 +0800

    Regenerate parser with tree-sitter v0.22.6 for Zed ABI compatibility

---
+++ bindings/c/tree-sitter-wfg.h
@@ -0,0 +1,16 @@
+#ifndef TREE_SITTER_WFG_H_
+#define TREE_SITTER_WFG_H_
+
+typedef struct TSLanguage TSLanguage;
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+const TSLanguage *tree_sitter_wfg(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // TREE_SITTER_WFG_H_
---
+++ bindings/python/tree_sitter_wfg/__init__.py
@@ -0,0 +1,5 @@
+"Wfg grammar for tree-sitter"
+
+from ._binding import language
+
+__all__ = ["language"]
